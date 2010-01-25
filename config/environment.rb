d540c7bed10b7217d02febe6050e6db9bcca16a6
Wes Morgan <cap10morgan@gmail.com>
2010-01-24 20:22:27 -0700

    added json gem requirement and db/* to .gitignore

--- config/environment.rb
+++ config/environment.rb
@@ -29,6 +29,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "geokit"
+  config.gem "json"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
